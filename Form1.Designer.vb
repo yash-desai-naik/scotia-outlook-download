87552429b939224477a90b76472dc593ceeeb817
yashdesai <yash.desai.naik@gmail.com>
2024-03-11 22:38:05 +0530

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,116 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtInterval = New System.Windows.Forms.TextBox()
+        Me.btnSelectDownloadPath = New System.Windows.Forms.Button()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.btnUpdateInterval = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'txtInterval
+        '
+        Me.txtInterval.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtInterval.Location = New System.Drawing.Point(57, 57)
+        Me.txtInterval.Name = "txtInterval"
+        Me.txtInterval.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.txtInterval.Size = New System.Drawing.Size(100, 37)
+        Me.txtInterval.TabIndex = 0
+        Me.txtInterval.Text = "10"
+        '
+        'btnSelectDownloadPath
+        '
+        Me.btnSelectDownloadPath.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSelectDownloadPath.Location = New System.Drawing.Point(57, 108)
+        Me.btnSelectDownloadPath.Name = "btnSelectDownloadPath"
+        Me.btnSelectDownloadPath.Size = New System.Drawing.Size(394, 48)
+        Me.btnSelectDownloadPath.TabIndex = 1
+        Me.btnSelectDownloadPath.Text = "Select Dowload Location"
+        Me.btnSelectDownloadPath.UseVisualStyleBackColor = True
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.ImageScalingSize = New System.Drawing.Size(24, 24)
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 201)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(498, 22)
+        Me.StatusStrip1.TabIndex = 2
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ToolStripStatusLabel1
+        '
+        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(0, 15)
+        '
+        'btnUpdateInterval
+        '
+        Me.btnUpdateInterval.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnUpdateInterval.Location = New System.Drawing.Point(264, 51)
+        Me.btnUpdateInterval.Name = "btnUpdateInterval"
+        Me.btnUpdateInterval.Size = New System.Drawing.Size(187, 51)
+        Me.btnUpdateInterval.TabIndex = 3
+        Me.btnUpdateInterval.Text = "Update Interval"
+        Me.btnUpdateInterval.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(164, 62)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(94, 29)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Seconds"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(498, 223)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnUpdateInterval)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.btnSelectDownloadPath)
+        Me.Controls.Add(Me.txtInterval)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
+        Me.Text = "Scotia - Outlook Email Downloader"
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtInterval As TextBox
+    Friend WithEvents btnSelectDownloadPath As Button
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents ToolStripStatusLabel1 As ToolStripStatusLabel
+    Friend WithEvents btnUpdateInterval As Button
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,147 @@
+﻿Imports Outlook = Microsoft.Office.Interop.Outlook
+Imports System.Timers
+Imports System.Text.RegularExpressions
+
+Public Class Form1
+    Private notifyIcon As System.Windows.Forms.NotifyIcon
+
+    Private WithEvents timer As New Timer()
+    Private outlookApp As Outlook.Application
+    Private downloadPath As String
+    Private interval As Integer
+
+    Public Sub New()
+        InitializeComponent()
+
+        ' Initialize timer
+        timer.Enabled = True
+        AddHandler timer.Elapsed, AddressOf Timer_Elapsed
+
+        ' Initialize Outlook application
+        outlookApp = New Outlook.Application()
+
+        ' Retrieve download path and interval from settings
+        downloadPath = My.Settings.DownloadPath
+        interval = My.Settings.Interval
+
+        ' Update UI with settings
+        ToolStripStatusLabel1.Text = downloadPath
+        txtInterval.Text = interval.ToString()
+
+        ' Set timer interval
+        timer.Interval = TimeSpan.FromSeconds(interval).TotalMilliseconds
+    End Sub
+
+    Private Sub Timer_Elapsed(sender As Object, e As ElapsedEventArgs)
+        ' Check for new emails
+        Dim inboxFolder As Outlook.Folder = outlookApp.Session.GetDefaultFolder(Outlook.OlDefaultFolders.olFolderInbox)
+        For Each item As Object In inboxFolder.Items
+            If TypeOf item Is Outlook.MailItem Then
+                Dim mailItem As Outlook.MailItem = DirectCast(item, Outlook.MailItem)
+                ' Process only unread emails
+                If Not mailItem.UnRead Then
+                    Continue For
+                End If
+
+                ' Define regular expression pattern to match subject
+                Dim pattern As String = "(RE: |RV: )?(?<Prefix>.+?) - Dodd-Frank DeMinimis Extract Request \| (?<StartDate>.+?) - (?<EndDate>.+?)( - SwapOne)?"
+
+                ' Check if the subject matches the pattern
+                Dim match As Match = Regex.Match(mailItem.Subject, pattern, RegexOptions.IgnoreCase)
+
+                If match.Success Then
+                    ' Extract information from the subject
+                    Dim prefix As String = match.Groups("Prefix").Value.Trim()
+                    Dim startDate As String = match.Groups("StartDate").Value.Trim()
+                    Dim endDate As String = match.Groups("EndDate").Value.Trim()
+
+                    ' Display a message box with subject details
+                    Dim message As String = $"Email received with subject:{Environment.NewLine}{Environment.NewLine}" &
+                                            $"Prefix: {prefix}{Environment.NewLine}" &
+                                            $"Start Date: {startDate}{Environment.NewLine}" &
+                                            $"End Date: {endDate}"
+                    MessageBox.Show(message, "New Email", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+                    ' Download attachments
+                    For Each attachment As Outlook.Attachment In mailItem.Attachments
+                        ' Save the attachment to the selected location
+                        attachment.SaveAsFile(System.IO.Path.Combine(downloadPath, attachment.FileName))
+                    Next
+
+                    ' Optionally, you can mark the email as read once processed
+                    mailItem.UnRead = False
+                    ' Save changes
+                    mailItem.Save()
+
+                End If
+
+
+            End If
+        Next
+    End Sub
+
+
+    Private Sub btnSelectDownloadPath_Click(sender As Object, e As EventArgs) Handles btnSelectDownloadPath.Click
+        ' Open folder browser dialog to select download location
+        Dim folderBrowserDialog As New FolderBrowserDialog()
+        If folderBrowserDialog.ShowDialog() = DialogResult.OK Then
+            downloadPath = folderBrowserDialog.SelectedPath
+            ToolStripStatusLabel1.Text = downloadPath
+
+            ' Save download path to settings
+            My.Settings.DownloadPath = downloadPath
+            My.Settings.Save()
+        End If
+    End Sub
+
+    Private Sub btnUpdateInterval_Click(sender As Object, e As EventArgs) Handles btnUpdateInterval.Click
+        ' Update interval based on the value entered in the interval textbox
+        Dim newInterval As Integer
+        If Integer.TryParse(txtInterval.Text, newInterval) AndAlso newInterval > 0 AndAlso newInterval <= Integer.MaxValue Then
+            ' Set timer interval
+            timer.Interval = TimeSpan.FromSeconds(newInterval).TotalMilliseconds
+
+            ' Save interval to settings
+            interval = newInterval
+            My.Settings.Interval = interval
+            My.Settings.Save()
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Initialize NotifyIcon
+        notifyIcon = New System.Windows.Forms.NotifyIcon()
+        notifyIcon.Icon = Me.Icon
+        notifyIcon.Text = Me.Text
+
+        ' Add a context menu to the NotifyIcon (optional)
+        Dim contextMenu As New System.Windows.Forms.ContextMenu()
+        contextMenu.MenuItems.Add("Restore", AddressOf RestoreForm)
+        contextMenu.MenuItems.Add("Exit", AddressOf ExitApplication)
+        notifyIcon.ContextMenu = contextMenu
+    End Sub
+
+    Private Sub Form1_Resize(sender As Object, e As EventArgs) Handles Me.Resize
+        If Me.WindowState = FormWindowState.Minimized Then
+            Me.Hide()
+            notifyIcon.Visible = True
+        End If
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        ' Ensure that the application exits properly by disposing of the NotifyIcon
+        notifyIcon.Dispose()
+    End Sub
+
+    Private Sub RestoreForm(sender As Object, e As EventArgs)
+        ' Show the form and hide the NotifyIcon when "Restore" is clicked
+        Me.Show()
+        Me.WindowState = FormWindowState.Normal
+        notifyIcon.Visible = False
+    End Sub
+
+    Private Sub ExitApplication(sender As Object, e As EventArgs)
+        ' Exit the application when "Exit" is clicked
+        Me.Close()
+    End Sub
+End Class
